0f99c96ebd28b7bfb15a0c673a8df9a0d76200a0
Ben Pang <benjinpang@gmail.com>
2016-05-08 15:56:02 -0700

    fix dialogs

--- public/main.js
+++ public/main.js
@@ -6,14 +6,6 @@ e.setOptions({
 	fontFamily:'dvsm',
 	fontSize:'13px'
 })
-res=ace.edit('res')
-res.getSession().setUseWorker(false)
-res.setTheme("ace/theme/monokai")
-res.getSession().setMode("ace/mode/javascript")
-res.setOptions({
-	fontFamily:'dvsm',
-	fontSize:'13px'
-})
 
 $('#run').click(function(){
 	$.get('/eval/'+$('#editor .ace_content').text(),function(x){
@@ -21,5 +13,13 @@ $('#run').click(function(){
 			title:'Result',
 			text:'<div id="res">'+x.r+'</div>'
 		})
+	})
+	res=ace.edit('res')
+	res.getSession().setUseWorker(false)
+	res.setTheme("ace/theme/monokai")
+	res.getSession().setMode("ace/mode/javascript")
+	res.setOptions({
+		fontFamily:'dvsm',
+		fontSize:'13px'
 	})
 });
