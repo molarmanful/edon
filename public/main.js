7751a475c1c05f047ea8a80942eab063977c7842
Ben Pang <benjinpang@gmail.com>
2016-05-08 16:00:12 -0700

    fix dialogs

--- public/main.js
+++ public/main.js
@@ -11,15 +11,10 @@ $('#run').click(function(){
 	$.get('/eval/'+$('#editor .ace_content').text(),function(x){
 		showDialog({
 			title:'Result',
-			text:'<div id="res">'+x.r+'</div>'
+			text:'<pre id="res" style="font-family:dvsm">'+x.r+'</pre>',
+			positive:{
+				title:'Edit'
+			}
 		})
-	})
-	res=ace.edit('res')
-	res.getSession().setUseWorker(false)
-	res.setTheme("ace/theme/monokai")
-	res.getSession().setMode("ace/mode/javascript")
-	res.setOptions({
-		fontFamily:'dvsm',
-		fontSize:'13px'
 	})
 });
