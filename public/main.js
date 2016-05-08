8d144332740de91cde2a41a17984f83ac5535b92
Ben Pang <benjinpang@gmail.com>
2016-05-08 16:14:38 -0700

    add blank val

--- public/main.js
+++ public/main.js
@@ -8,7 +8,7 @@ e.setOptions({
 })
 
 $('#run').click(function(){
-	$.get(encodeURI('/eval/'+e.getValue()),function(x){
+	$.get(encodeURI('/eval/'+(e.getValue()||'undefined')),function(x){
 		showDialog({
 			title:'Result',
 			text:'<pre id="res" style="font-family:dvsm">'+x.r+'</pre>',
