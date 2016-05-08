c18ea6eec646418573f72e34055f471b1feead37
Ben Pang <benjinpang@gmail.com>
2016-05-08 15:54:28 -0700

    add dialog

--- public/main.js
+++ public/main.js
@@ -6,9 +6,20 @@ e.setOptions({
 	fontFamily:'dvsm',
 	fontSize:'13px'
 })
+res=ace.edit('res')
+res.getSession().setUseWorker(false)
+res.setTheme("ace/theme/monokai")
+res.getSession().setMode("ace/mode/javascript")
+res.setOptions({
+	fontFamily:'dvsm',
+	fontSize:'13px'
+})
 
 $('#run').click(function(){
 	$.get('/eval/'+$('#editor .ace_content').text(),function(x){
-		alert(x.r)
+		showDialog({
+			title:'Result',
+			text:'<div id="res">'+x.r+'</div>'
+		})
 	})
 });
