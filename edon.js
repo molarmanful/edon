96adce5f0b759a91d4e03b746fd6997db011556f
Ben Pang <benjinpang@gmail.com>
2016-05-11 06:30:07 -0700

    debugging

--- edon.js
+++ edon.js
@@ -59,3 +59,4 @@ app.get('/eval/:x',(x,y)=>{
 	})
 })
 app.listen(process.env.OPENSHIFT_NODEJS_PORT||8080,process.env.OPENSHIFT_NODEJS_IP||'127.0.0.1')
+console.log(process.env.OPENSHIFT_NODEJS_VERSION_URL)
